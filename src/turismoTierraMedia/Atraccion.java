f05d72688832cfca81f0e8d4093013b7ffa905e7
esteban <eptheclash@gmail.com>
2021-08-29 15:04:03 -0300

    Se agrega constructor, atributos y getters

--- src/turismoTierraMedia/Atraccion.java
+++ src/turismoTierraMedia/Atraccion.java
@@ -2,4 +2,25 @@ package turismoTierraMedia;
 
 public class Atraccion {
 
+	private int costo;
+	private int tiempo;
+	private int cupo;
+	public Atraccion(int costo, int tiempo, int cupo) {
+		super();
+		this.costo = costo;
+		this.tiempo = tiempo;
+		this.cupo = cupo;
+	}
+
+	protected int getCosto() {
+		return costo;
+	}
+
+	protected int getTiempo() {
+		return tiempo;
+	}
+
+	protected int getCupo() {
+		return cupo;
+	}
 }
